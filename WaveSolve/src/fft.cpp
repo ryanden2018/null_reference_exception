8daf29e36ca3e211593a0bbc4b0e1ac36ffea284
Ryan Denlinger <denlinger.ryan@gmail.com>
2021-03-16 23:54:16 -0500

    wip: fft. both 2d, 3d fft and ifft implemented. ready to test.

--- WaveSolve/src/fft.cpp
+++ WaveSolve/src/fft.cpp
@@ -104,6 +104,15 @@ private:
         return result;
     }
 
+    void concat_back(complex_vector &front, complex_vector const &back)
+    {
+        auto end = back.end();
+        for (auto it = back.begin(); it != end; it++)
+        {
+            front.push_back(*it);
+        }
+    }
+
     bool is_a_power_of_two(int N)
     {
         if (N <= 0)
@@ -118,6 +127,36 @@ private:
             k++;
         }
         return N == two_to_the_power_of(k);
+    }
+
+    int greatest_number_with_square_not_greater_than(int M)
+    {
+        if (M <= 0)
+        {
+            return 0;
+        }
+        for (int k = 1; k <= M; k++)
+        {
+            if ((k + 1) * (k + 1) > M)
+            {
+                return k;
+            }
+        }
+    }
+
+    int greatest_number_with_cube_not_greater_than(int M)
+    {
+        if (M <= 0)
+        {
+            return 0;
+        }
+        for (int k = 1; k <= M; k++)
+        {
+            if ((k + 1) * (k + 1) * (k + 1) > M)
+            {
+                return k;
+            }
+        }
     }
 
     complex_vector &conj_in_place(complex_vector &in_vector)
@@ -141,10 +180,53 @@ private:
         return in_vector;
     }
 
-    std::unique_ptr<complex_vector> fft_helper(complex_vector const &in_vector, int &err)
+    complex_vector &transpose_in_place(complex_vector &in_vector, int N)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                auto tmp = in_vector[N * i + j];
+                in_vector[N * i + j] = in_vector[N * j + i];
+                in_vector[N * j + i] = tmp;
+            }
+        }
+        return in_vector;
+    }
+
+    // Cycle the three coordinates via two successive reflections (likely sub-optimal).
+    complex_vector &cycle_3d_in_place(complex_vector &in_vector, int N)
+    {
+        for (int k = 0; k < N; k++)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    auto tmp = in_vector[N * N * k + N * j + i];
+                    in_vector[N * N * k + N * j + i] = in_vector[N * N * k + N * i + j];
+                    in_vector[N * N * k + N * i + j] = tmp;
+                }
+            }
+        }
+        for (int j = 0; j < N; j++)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    auto tmp = in_vector[N * N * k + N * j + i];
+                    in_vector[N * N * k + N * j + i] = in_vector[N * N * i + N * j + k];
+                    in_vector[N * N * i + N * j + k] = tmp;
+                }
+            }
+        }
+        return in_vector;
+    }
+
+    std::unique_ptr<complex_vector> fft_helper(complex_vector const &in_vector, int N, int &err)
     {
         err = 0;
-        int N = in_vector.size();
         if (N == 1)
         {
             auto out_vector = std::make_unique<complex_vector>();
@@ -154,19 +236,17 @@ private:
         else
         {
             auto sliced = slice(in_vector, in_vector.begin(), in_vector.end(), 2);
-            auto front = fft_helper(*sliced, err);
+            auto front = fft_helper(*sliced, N / 2, err);
             if (err != 0)
             {
-                auto out_vector = std::make_unique<complex_vector>();
-                return out_vector;
+                return std::make_unique<complex_vector>();
             }
 
             sliced = slice(in_vector, in_vector.begin() + 1, in_vector.end(), 2);
-            auto back = fft_helper(*sliced, err);
+            auto back = fft_helper(*sliced, N / 2, err);
             if (err != 0)
             {
-                auto out_vector = std::make_unique<complex_vector>();
-                return out_vector;
+                return std::make_unique<complex_vector>();
             }
 
             auto front_end = front->end();
@@ -183,19 +263,73 @@ private:
                 *it2 = tmp - twiddle_factor * (*it2);
             }
 
-            auto out_vector = concat(*front, *back);
-            return out_vector;
+            return concat(*front, *back);
         }
     }
 
-    std::unique_ptr<complex_vector> ifft_helper(complex_vector const &in_vector, int &err)
+    std::unique_ptr<complex_vector> ifft_helper(complex_vector const &in_vector, int N, int &err)
     {
+        err = 0;
         auto in_vector_cpy_ptr = std::make_unique<complex_vector>(in_vector);
         conj_in_place(*in_vector_cpy_ptr);
-        auto fft_in_vector_cpy_ptr = fft_helper(*in_vector_cpy_ptr, err);
+        auto fft_in_vector_cpy_ptr = fft_helper(*in_vector_cpy_ptr, N, err);
         conj_in_place(*fft_in_vector_cpy_ptr);
         divide_by_length_in_place(*fft_in_vector_cpy_ptr);
         return fft_in_vector_cpy_ptr;
+    }
+
+    std::unique_ptr<complex_vector> fftd_helper_1d(complex_vector const &in_vector, int N, int &err, bool inverse)
+    {
+        err = 0;
+        auto start = in_vector.begin();
+        auto stop = advance(in_vector, start, N);
+        auto end = in_vector.end();
+        auto out_vector = std::make_unique<complex_vector>();
+        while (stop != end)
+        {
+            auto row = slice(in_vector, start, stop, 1);
+            auto fft_row = inverse ? ifft_helper(*row, N, err) : fft_helper(*row, N, err);
+            if (err != 0)
+            {
+                break;
+            }
+            concat_back(*out_vector, *fft_row);
+            stop = advance(in_vector, stop, N);
+        }
+        return out_vector;
+    }
+
+    std::unique_ptr<complex_vector> fft2_helper(complex_vector const &in_vector, int N, int &err, bool inverse)
+    {
+        err = 0;
+        auto transformed_first_dim = fftd_helper_1d(in_vector, N, err, inverse);
+        if (err != 0)
+        {
+            return transformed_first_dim;
+        }
+        transpose_in_place(*transformed_first_dim, N);
+        auto transformed_second_dim = fftd_helper_1d(*transformed_first_dim, N, err, inverse);
+        transpose_in_place(*transformed_second_dim, N);
+        return transformed_second_dim;
+    }
+
+    std::unique_ptr<complex_vector> fft3_helper(complex_vector const &in_vector, int N, int &err, bool inverse)
+    {
+        err = 0;
+        auto transformed_first_dim = fftd_helper_1d(in_vector, N, err, inverse);
+        if (err != 0)
+        {
+            return transformed_first_dim;
+        }
+        cycle_3d_in_place(*transformed_first_dim, N);
+        auto transformed_second_dim = fftd_helper_1d(*transformed_first_dim, N, err, inverse);
+        if (err != 0)
+        {
+            return transformed_second_dim;
+        }
+        auto transformed_third_dim = fftd_helper_1d(*transformed_second_dim, N, err, inverse);
+        cycle_3d_in_place(*transformed_third_dim, N);
+        return transformed_third_dim;
     }
 
 public:
@@ -224,21 +358,77 @@ public:
 
     std::unique_ptr<complex_vector> fft(complex_vector const &in_vector, int &err)
     {
-        if (!is_a_power_of_two(in_vector.size()))
+        err = 0;
+        int N = in_vector.size();
+        if (!is_a_power_of_two(N))
         {
             err = 1;
             return std::make_unique<complex_vector>();
         }
-        return fft_helper(in_vector, err);
+        return fft_helper(in_vector, N, err);
     }
 
     std::unique_ptr<complex_vector> ifft(complex_vector const &in_vector, int &err)
     {
-        if (!is_a_power_of_two(in_vector.size()))
+        err = 0;
+        int N = in_vector.size();
+        if (!is_a_power_of_two(N))
         {
             err = 1;
             return std::make_unique<complex_vector>();
         }
-        return ifft_helper(in_vector, err);
+        return ifft_helper(in_vector, N, err);
+    }
+
+    std::unique_ptr<complex_vector> fft2(complex_vector const &in_vector, int &err)
+    {
+        err = 0;
+        int M = in_vector.size();
+        int N = greatest_number_with_square_not_greater_than(M);
+        if (M != N * N || !is_a_power_of_two(N))
+        {
+            err = 1;
+            return std::make_unique<complex_vector>();
+        }
+        return fft2_helper(in_vector, N, err, false);
+    }
+
+    std::unique_ptr<complex_vector> ifft2(complex_vector const &in_vector, int &err)
+    {
+        err = 0;
+        int M = in_vector.size();
+        int N = greatest_number_with_square_not_greater_than(M);
+        if (M != N * N || !is_a_power_of_two(N))
+        {
+            err = 1;
+            return std::make_unique<complex_vector>();
+        }
+        return fft2_helper(in_vector, N, err, true);
+    }
+
+    std::unique_ptr<complex_vector> fft3(complex_vector const &in_vector, int &err)
+    {
+        err = 0;
+        int M = in_vector.size();
+        int N = greatest_number_with_cube_not_greater_than(M);
+        if (M != N * N * N || !is_a_power_of_two(N))
+        {
+            err = 1;
+            return std::make_unique<complex_vector>();
+        }
+        return fft3_helper(in_vector, N, err, false);
+    }
+
+    std::unique_ptr<complex_vector> ifft3(complex_vector const &in_vector, int &err)
+    {
+        err = 0;
+        int M = in_vector.size();
+        int N = greatest_number_with_cube_not_greater_than(M);
+        if (M != N * N * N || !is_a_power_of_two(N))
+        {
+            err = 1;
+            return std::make_unique<complex_vector>();
+        }
+        return fft3_helper(in_vector, N, err, true);
     }
 };
--- WaveSolve/src/fft.hpp
+++ WaveSolve/src/fft.hpp
@@ -29,22 +29,48 @@ public:
     void precompute_twiddle_factors(int N);
 
     /**
-     * Compute the DFT of in_vector having length a power of two,
-     * the length being determined at compile time.
+     * Compute the DFT of in_vector having length a power of two.
      * 
      * err is 0 if success.
      */
     std::unique_ptr<std::vector<std::complex<double>>> fft(std::vector<std::complex<double>> const &in_vector, int &err);
 
     /**
-     * Compute the inverse DFT of in_vector having length a power of two,
-     * the length being determined at compile time.
+     * Compute the inverse DFT of in_vector having length a power of two.
      * 
      * Current implementation is less efficient than fft() since ifft() uses fft() along with an additional copy.
      * 
      * err is 0 if success.
      */
     std::unique_ptr<std::vector<std::complex<double>>> ifft(std::vector<std::complex<double>> const &in_vector, int &err);
+
+    /**
+     * Compute the N by N 2-D DFT of in_vector, N being a power of two.
+     * 
+     * err is 0 if success.
+     */
+    std::unique_ptr<std::vector<std::complex<double>>> fft2(std::vector<std::complex<double>> const &in_vector, int &err);
+
+    /**
+     * Compute the N by N 2-D inverse DFT of in_vector, N being a power of two.
+     * 
+     * err is 0 if success.
+     */
+    std::unique_ptr<std::vector<std::complex<double>>> ifft2(std::vector<std::complex<double>> const &in_vector, int &err);
+
+    /**
+     * Compute the N by N by N 3-D DFT of in_vector, N being a power of two.
+     * 
+     * err is 0 if success.
+     */
+    std::unique_ptr<std::vector<std::complex<double>>> fft3(std::vector<std::complex<double>> const &in_vector, int &err);
+
+    /**
+     * Compute the N by N by N 3-D inverse DFT of in_vector, N being a power of two.
+     * 
+     * err is 0 if success.
+     */
+    std::unique_ptr<std::vector<std::complex<double>>> ifft3(std::vector<std::complex<double>> const &in_vector, int &err);
 };
 
 #endif
